72456d28ac85ea9b92ced7bcce08fe43c5c87257
TUSHAR SHARMA <tusharsharma991193@gmail.com>
2026-03-26 20:51:36 +0530

    Delete part4_modules directory

--- part4_modules/db/book.js
+++
@@ -1,9 +0,0 @@
-const BOOKS = [
-    { id: 1, title: 'book One', author: 'author One' },
-    { id: 2, title: 'book Two', author: 'author Two' },
-];
-
-module.exports = BOOKS;
-
-// named export  here ! 
-// we exported the books array from this module  , taaki hum isse book.routes.js ko import karke use kar sake ,  then hum uske saare routes me ise access kar sakte hai  
